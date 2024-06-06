61c7c793a4a745bae09a761b0e086defa63151ac
NinnaMTR <marianaramalheteebirdll@gmail.com>
2024-06-06 17:00:16 +0100

    Added search engine functionality to the project

--- src/app.js
+++ src/app.js
@@ -1,3 +1,19 @@
+function refreshWeather(response) {
+  let cityElement = document.querySelector("#city");
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+//API function
+function searchCity(city) {
+  let apiKey = "390db6dco38711524t5d45ba8dc8afff";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
 
@@ -5,7 +21,12 @@ function handleSearchSubmit(event) {
 
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+//first search for Lisbon
+searchCity("Lisbon");
